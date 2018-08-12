cfc0af04b0b85928fec64f7c7a3a835aa255cef8
Yan Minzhi <yanmingzhi@elastos.org>
2018-08-12 10:30:17 +0800

    Modify the text parser.

--- bin/mindgen.js
+++ bin/mindgen.js
@@ -9,25 +9,28 @@ var argv = require('yargs')
     .alias('t', 'text')
     .alias('m', 'markdown')
     .alias('s', 'source')
+    .alias('p', 'print-text-output-only')
     .argv;
 
 var text = fs.readFileSync(argv.s, 'utf-8');
 
 var headings;
-if(argv.m)
+if (argv.m)
     headings = parseMarkdown(text);
 else //default we parse text file
     headings = parse(text);
 var root = transform(headings);
 
-console.log(root);
+if (argv.p) {
+    console.log(root);
+} else {
+    fs.writeFileSync('result.json', JSON.stringify(root));
 
-fs.writeFileSync('result.json', JSON.stringify(root));
-
-//replace gtor source so that we can view the result on browser by simplely open index.html
-exec('mv result.json ../examples/gtor.json', function(err, stdout, stderr){
-    if(err)
-        console.log('move failed: ' + err);
-    else
-        console.log('move success!');
-})
+    //replace gtor source so that we can view the result on browser by simplely open index.html
+    exec('mv result.json ../examples/gtor.json', function (err, stdout, stderr) {
+        if (err)
+            console.log('move failed: ' + err);
+        else
+            console.log('move success!');
+    })
+}
--- parse.txtmap.js
+++ parse.txtmap.js
@@ -1,18 +1,35 @@
 module.exports = function(text) {
   var lines = text.split('\n');
-
+  
   var entries = [];
-  for (var i = 0; i < lines.length; i++) {
+  //let first line to be title defaultly
+  entries.push({
+    depth: 1,
+    line: 0,
+    name: lines[0]
+  });
+
+  var firstIndent = true;
+  var indentLength = 1;
+
+  for (var i = 1; i < lines.length; i++) {
     var match = /^(\s*)(.+)$/.exec(lines[i]);
     if (!match) {
       continue;
     }
+
+    //to catch tab and other space situation
     var isSpaces = match[1].length > 0 && match[1][0] === ' ';
     var depth = match[1].length;
     if (isSpaces) {
-      depth /= 2;
+      if (firstIndent) {
+        //decide indent length by first spaces depth, so make sure you keep the same indent style
+        indentLength = depth;
+        firstIndent = false;
+      }
+      depth /= indentLength;
     }
-    depth += 1; // ensure depth is min 1
+    depth += 2; // ensure depth is min 2
 
     entries.push({
       depth: depth,
